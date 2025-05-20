adc88cb10254e7fbc07d6495981c5f3214c63cd1
Åsa Johansson <asa.j.1989@gmail.com>
2025-05-20 12:49:10 +0200

    Migration to database.

---
+++ WebApi/Migrations/20250520104829_AddedColumnToEventTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedColumnToEventTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatsLeft",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Venue",
+                table: "Events",
+                type: "nvarchar(30)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Venue",
+                table: "Events");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatsLeft",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
