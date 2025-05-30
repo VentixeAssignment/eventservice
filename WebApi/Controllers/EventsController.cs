a189ca3ad9d0ac52790fc3cda247d75d005b1166
Åsa Johansson <asa.j.1989@gmail.com>
2025-05-30 19:42:33 +0200

    Add method to get one event in eventscontroller.

--- WebApi/Controllers/EventsController.cs
+++ WebApi/Controllers/EventsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Dto;
+using WebApi.Models;
 using WebApi.Services;
 
 namespace WebApi.Controllers
@@ -20,9 +21,29 @@ namespace WebApi.Controllers
             if (events == null || events.DataList == null)
                 return BadRequest("No events returned.");
 
-                return events!.DataList!.Any()
-                    ? Ok(events.DataList)
-                    : NotFound("No events fount.");
+            return events!.DataList!.Any()
+                ? Ok(events.DataList)
+                : NotFound("No events found.");
+        }
+
+        [HttpGet]
+        [Route("event{id}")]
+        public async Task<IActionResult> GetEventAsync([FromRoute] string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return BadRequest("Id cannot be null or empty.");
+
+            var result = await _eventService.GetOneAsync(id);
+
+            if (result.StatusCode == 409)
+                return Conflict(new { result.ErrorMessage });
+
+            if (result.StatusCode == 400)
+                return BadRequest(new { result.ErrorMessage });
+
+            return result.Success
+                ? Created(string.Empty, new { result.Data })
+                : StatusCode(result.StatusCode, new { result.ErrorMessage });
         }
 
 
